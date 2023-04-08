d93f34dbda67811e16da25a3cc9aece76f452ce1
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-04-09 01:40:26 +0300

    fix: resolve navigation link bug

--- shelter/assets/scripts/burger.js
+++ shelter/assets/scripts/burger.js
@@ -4,20 +4,26 @@ const menuButton = navigation.querySelector('.main-navigation__burger-button');
 const modalBackground = document.querySelector('.modal-background');
 
 menuButton.addEventListener('click', switchMenuState);
-modalBackground.addEventListener ('click', switchMenuState);
+modalBackground.addEventListener ('click', () => {
+  navigation.classList.add('main-navigation--contracted');
+  navigation.classList.remove('main-navigation--expanded');
+  switchModalBackground();
+});
 navigation.addEventListener('click', (evt) => {
   const currentPageLink = navigation.querySelector('.navitation-links__item--active .link--navigation');
   const link = evt.target.closest('.link--navigation');
 
-  if (link === currentPageLink) {
-    evt.preventDefault();
-  }
   const isVisible = window.getComputedStyle(menuButton).display !== 'none';
   if (!isVisible) {
     return;
   }
+
   if (!link) {
     return;
+  }
+
+  if (link === currentPageLink) {
+    evt.preventDefault();
   }
 
   switchMenuState();
@@ -42,7 +48,7 @@ function switchModalBackground() {
   if (navigation.classList.contains('main-navigation--expanded')) {
     modalBackground.style.display = 'block';
     document.documentElement.style.overflow = 'hidden';
-    document.documentElement.style.paddingRight = '16px';
+    document.documentElement.style.paddingRight = '0px';
   } else {
     modalBackground.style.display = 'none';
     document.documentElement.style.overflow = 'auto';
