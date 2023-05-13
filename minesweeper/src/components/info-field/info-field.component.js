5107f73a76522890ad811a57796ccd41dd0e4412
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-05-13 12:29:48 +0300

    feat: add info field component

---
+++ minesweeper/src/components/info-field/info-field.component.js
@@ -0,0 +1,23 @@
+import './info-field.component.scss';
+import { BaseComponent } from '../base.component';
+
+export class InfoFieldComponent extends BaseComponent {
+  constructor({ labelText, className = [] }) {
+    super({ tagName: 'p', className: [className, 'info-field'] });
+    const labelComponent = new BaseComponent({
+      tagName: 'span',
+      textContent: labelText,
+      className: 'info-field__label',
+    });
+    this.valueComponent = new BaseComponent({
+      tagName: 'output',
+      textContent: '00',
+      className: 'info-field__value',
+    });
+    this.append(labelComponent, this.valueComponent);
+  }
+
+  setValue(value) {
+    this.valueElement = value;
+  }
+}
