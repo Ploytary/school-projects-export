1b84faed21e7b119f978fa6edc67b5ff938e86dd
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-05-14 16:43:27 +0300

    fix: change svg enum name, add base class to svg component

--- minesweeper/src/components/svg.component.js
+++ minesweeper/src/components/svg.component.js
@@ -2,7 +2,7 @@ import { BaseComponent } from './base.component';
 import { createElementFromTemplate } from '../utils/create-element-from-template';
 
 export class SVGComponent extends BaseComponent {
-  constructor({ className, template }) {
+  constructor({ className = 'svg-icon', template }) {
     super({});
     this.node = createElementFromTemplate(template);
     this.node.classList.add(className);
