d0a5eb7c571b0f4bf9680f81db1b319774220f93
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-06-12 11:36:06 +0300

    fix: add news api proxy

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '522a81d439a5413aa123326fd3b13d12', // получите свой ключ https://newsapi.org/
         });
     }
