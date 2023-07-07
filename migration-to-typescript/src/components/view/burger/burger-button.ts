e1a9d04e3a84aa7e842705269999495ef437e155
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-07-07 23:23:11 +0300

    fix: menu items & button click handler

--- migration-to-typescript/src/components/view/burger/burger-button.ts
+++ migration-to-typescript/src/components/view/burger/burger-button.ts
@@ -1,19 +1,24 @@
 import './burger-button.css';
 import { getGuaranteedElement } from '../../../utils/common';
 
-const menuButton: HTMLElement = getGuaranteedElement(document, '.burger-button');
+const burgerButton: HTMLElement = getGuaranteedElement(document, '.burger-button');
+const buttonImages: NodeListOf<Element> = burgerButton.querySelectorAll('.burger-button__image');
 const menuElement: HTMLElement = getGuaranteedElement(document, '.sources');
-document.body.addEventListener('click', menuClickHandler);
 
-function menuClickHandler(evt: MouseEvent): void {
-    const target: HTMLElement = evt.target as HTMLElement;
-    const button: HTMLElement | null = target.closest('.burger-button');
-    const sourceName: HTMLElement | null = target.closest('.source__item');
-    const menuArea: HTMLElement | null = target.closest('.sources');
-    const buttonImages = menuButton.querySelectorAll('.burger-button__image');
+const menuClickHandler: () => void = () => {
+    menuElement.classList.toggle('sources--expanded');
+    buttonImages.forEach((image) => image.classList.toggle('burger-button__image--visible'));
+};
 
-    if (button || sourceName || !menuArea) {
-        menuElement.classList.toggle('sources--expanded');
-        buttonImages.forEach((image) => image.classList.toggle('burger-button__image--visible'));
+burgerButton.addEventListener('click', menuClickHandler);
+
+menuElement.addEventListener('click', (evt) => {
+    if (!(evt.target instanceof HTMLElement)) {
+        return;
     }
-}
+    const listItem = evt.target.closest('.source__item');
+    if (!listItem) {
+        return;
+    }
+    menuClickHandler();
+});
